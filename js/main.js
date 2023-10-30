6da0fc312266955f8a6cf2354195227eeb9c01d2
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-10-30 21:56:16 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
 for (let i = 1; i <= 100; i++) {
 
-    
-    
     if(i % 5 == 0 && i % 3 == 0){
-        console.log("fizzbuzz")
+        const node = document.createElement("li");
+        const textnode = document.createTextNode("fizzbuzz");
+        node.appendChild(textnode);
+        document.getElementById("myList").appendChild(node);
+        node.classList.add("col-2");
         continue;
     }
     
     if(i % 3 == 0){
-     console.log("fizz")
-     continue;
-    }
- 
-    if(i % 5 == 0){
-     console.log("buzz")
-     continue;
+        const node = document.createElement("li");
+        const textnode = document.createTextNode("fizz");
+        node.appendChild(textnode);
+        document.getElementById("myList").appendChild(node);
+        node.classList.add("col-2");
+        continue;
     }
     
-   console.log(i)
+    if(i % 5 == 0){
+        const node = document.createElement("li");
+        const textnode = document.createTextNode("buzz");
+        node.appendChild(textnode);
+        document.getElementById("myList").appendChild(node);
+        node.classList.add("col-2");
+        continue;
+    }
+    
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(i);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+    node.classList.add("col-2");
  }
