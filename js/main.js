94b072a47360d4e0147168524b2e4b3213723531
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-10-30 21:13:35 +0100

    fizzbuzz done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 100; i++) {
+
+    
+    
+    if(i % 5 == 0 && i % 3 == 0){
+        console.log("fizzbuzz")
+        continue;
+    }
+    
+    if(i % 3 == 0){
+     console.log("fizz")
+     continue;
+    }
+ 
+    if(i % 5 == 0){
+     console.log("buzz")
+     continue;
+    }
+    
+   console.log(i)
+ }
